d5352ee5d70d2b10fbeb41dbb18ccc0d81f30e1b
Yoni <yoni@cyberillium.com>
2024-06-15 19:38:15 +0300

    Updated design

--- popup.js
+++ popup.js
@@ -1,38 +1,27 @@
-/**
- * Event listener that triggers when the DOM content is loaded.
- * Initializes the popup and sets up event handlers.
- *
- * @event DOMContentLoaded
- */
 document.addEventListener('DOMContentLoaded', () => {
     const toggleButton = document.getElementById('toggle');
-    const urlInput = document.getElementById('url');
+    const statusElement = document.getElementById('status');
+    const urlElement = document.getElementById('url');
+    const defaultUrl = 'https://sheilta.apps.openu.ac.il/pls/dmyopt2/myop.myop_screen';
 
-    /**
-     * Updates the toggle button text and click event handler based on the action.
-     *
-     * @function updateButton
-     * @param {string} action - The action to perform ('start' or 'stop').
-     */
     function updateButton(action) {
         if (action === 'stop') {
             toggleButton.classList.remove('run');
             toggleButton.classList.add('stop');
             toggleButton.textContent = 'Stop';
-            document.getElementById('status').innerText = 'Running';
+            statusElement.innerText = 'Running';
         } else {
             toggleButton.classList.remove('stop');
             toggleButton.classList.add('run');
             toggleButton.textContent = 'Start';
-            document.getElementById('status').innerText = 'Stopped';
+            statusElement.innerText = 'Stopped';
         }
     }
 
-    // Get the current status from the background script
     function getStatus() {
         chrome.runtime.sendMessage({ action: 'getStatus' }, (response) => {
-            urlInput.value = response.url;
-            if (response.status === 'running') {
+            urlElement.value = response.url || defaultUrl;
+            if (response.status === 'Running') {
                 updateButton('stop');
             } else {
                 updateButton('start');
@@ -40,16 +29,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    // Initialize the popup with the current status
     getStatus();
 
     toggleButton.addEventListener('click', () => {
-        const url = urlInput.value || 'https://sheilta.apps.openu.ac.il/';
-        if (toggleButton.textContent === 'Start') {
+        const currentState = statusElement.innerText.includes('Running') ? 'Running' : 'Stopped';
+        const url = urlElement.value || defaultUrl;
+
+        if (currentState === 'Stopped') {
             chrome.runtime.sendMessage({ action: 'start longpoll', url: url }, (response) => {
                 updateButton('stop');
             });
-        } else if (toggleButton.textContent === 'Stop') {
+        } else if (currentState === 'Running') {
             chrome.runtime.sendMessage({ action: 'stop longpoll' }, (response) => {
                 updateButton('start');
             });
